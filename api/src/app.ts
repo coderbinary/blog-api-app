62f5a425a38b3c02da4f1ffec0de92d881730887
Coder Binary <162278815+coderbinary@users.noreply.github.com>
2026-08-19 07:49:12 +0530

    Add Cookie Parser and Set it up

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,12 +3,14 @@ import cors from "cors";
 import { notFound } from "./middlewares/notFound.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { apiRouter } from "./routes/apiRouter.js";
+import cookieParser from "cookie-parser";
 
 export const createApp = () => {
   const app = express();
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({extended: true}));
+  app.use(cookieParser());
 
   app.use("/api",apiRouter);
   app.use(notFound);
